5f12642ed0459ef9cde297e902d3e3d11db39a23
hjxwhy <990719504@qq.com>
2022-10-18 02:21:38 +0800

    add requirements.txt

--- vidar/utils/setup.py
+++ vidar/utils/setup.py
@@ -235,7 +235,7 @@ def setup_metrics(cfg):
         'depth': DepthEvaluation,
     }
 
-    available_tasks = [key for key in cfg.__dict__.keys() if key is not 'tasks']
+    available_tasks = [key for key in cfg.__dict__.keys() if key != 'tasks']
     requested_tasks = cfg_has(cfg, 'tasks', available_tasks)
     tasks = [task for task in available_tasks if task in requested_tasks and task in methods]
 
